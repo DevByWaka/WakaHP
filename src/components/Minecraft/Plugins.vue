dbfafca71b969f36d580ea6316f6564b577f5ecd
DevByWaka <f.scarlet495@gmail.com>
2023-02-17 16:21:42 +0900

    create features

--- src/components/Minecraft/Plugins.vue
+++ src/components/Minecraft/Plugins.vue
@@ -20,7 +20,8 @@
       return {
         list: [
           {img:'https://cdn.vuetifyjs.com/images/cards/sunshine.jpg',title:'座れるぞ',subtitle:'gsit',text:"[/sit]座る(階段に触れても座れる)\n[/crawl]泳ぐ\n[/spin]回る"},
-          {img:'https://cdn.vuetifyjs.com/images/cards/sunshine.jpg',title:'Title2',subtitle:'SubTitle2',text:'Text2sssssssssss'}
+          {img:'https://cdn.vuetifyjs.com/images/cards/sunshine.jpg',title:'瞬時にテレポート',subtitle:'sethome',text:'[/sethome 場所の名前]で今いるところを登録して[/homes]から選べる'},
+          {img:'https://cdn.vuetifyjs.com/images/cards/sunshine.jpg',title:'瞬時にテレポート',subtitle:'sethome',text:'[/sethome 場所の名前]で今いるところを登録して[/homes]から選べる'}
         ]
       }
     }

51269ba445075c131b626dca6e0421efe64d984b
Kow1210 <f.scarlet495@gmail.com>
2023-02-18 22:53:57 +0900

    fix

--- src/views/Minecraft.vue
+++ src/views/Minecraft.vue
@@ -11,33 +11,14 @@
         </v-parallax>
         <v-row class="d-flex align-center justify-center">
         <v-col cols="auto">
-          <v-btn color="red-darken-1" min-width="228" rel="noopener noreferrer" size="x-large" variant="flat" href="minecraft://?addExternalServer=%20Wakaサーバー|waka1210mc.ddns.net:25565">
+          <v-btn color="red-darken-1" min-width="228" rel="noopener noreferrer" size="x-large" variant="flat" href="minecraft:///?addExternalServer=+Waka%E3%82%B5%E3%83%BC%E3%83%90%E3%83%BC|waka1210mc.ddns.net:25565">
             <v-icon icon="mdi:mdi-checkbox-marked" size="large" start/>
             サーバーを追加する
           </v-btn>
         </v-col>
 
         <v-col cols="auto">
-          <v-btn color="green-darken-1" min-width="228" rel="noopener noreferrer" size="x-large" variant="flat" to="/Minecraft" >
-            <v-icon icon="mdi:mdi-minecraft" size="large" start/>
-            マインクラフト
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn color="deep-purple-darken-1" min-width="228" rel="noopener noreferrer" size="x-large" variant="flat" to="/SNS" >
-            <youtube-icon style="fill:white; margin-right: 15px;" class="custom-class"></youtube-icon>
-            <twitter-icon style="fill:white; margin-right: 15px;" class="custom-class"></twitter-icon>
-            <discord-icon style="fill:white; margin-right: 15px;" class="custom-class"></discord-icon>
-            SNS
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn color="#1E88E5" min-width="228" rel="noopener noreferrer" size="x-large" variant="flat" to="/Contact" >
-            <v-icon icon="mdi:mdi-gmail" size="large" start />
-            お問い合わせ
-          </v-btn>
+          <v-img contain height="200px" src="@/assets/WO.png" />
         </v-col>
       </v-row>
         <div class="py-5" />

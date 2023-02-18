68958bd3867c667a508be4df48c785dda7359840
Kow1210 <f.scarlet495@gmail.com>
2023-02-18 23:44:11 +0900

    fix

--- src/views/Minecraft.vue
+++ src/views/Minecraft.vue
@@ -5,22 +5,47 @@
         <!--<v-img contain height="300" src="@/assets/WO.png" />-->
         <v-parallax height="100%" src='@/assets/Minecraft/features/center.png'>
           <div style="position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%);">
-            <h1 class="text-h4 font-weight-bold">マインクラフト<br>Wakaサーバー</h1>
+            <h1 class="text-h5 font-weight-bold">マイクラ<br>Wakaサーバー</h1>
             <div class="text-body-5 font-weight-light mb-n1">Waka Game</div>
           </div>
         </v-parallax>
-        <v-row class="d-flex align-center justify-center">
-        <v-col cols="auto">
-          <v-btn color="red-darken-1" min-width="228" rel="noopener noreferrer" size="x-large" variant="flat" href="minecraft:///?addExternalServer=+Waka%E3%82%B5%E3%83%BC%E3%83%90%E3%83%BC|waka1210mc.ddns.net:25565">
-            <v-icon icon="mdi:mdi-checkbox-marked" size="large" start/>
-            サーバーを追加する
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-img contain height="200px" src="@/assets/WO.png" />
-        </v-col>
-      </v-row>
+        <div class="py-5" />
+        <!--参加方法-->
+        <div style="background-color: #333" class="px-5">
+          <v-row class="d-flex align-center justify-center">
+            <v-col cols="auto"><!--統合版-->
+              <v-btn color="teal-darken-1" min-width="228" rel="noopener noreferrer" size="x-large" variant="flat" href="minecraft:///?addExternalServer=+Waka%E3%82%B5%E3%83%BC%E3%83%90%E3%83%BC|waka1210mc.ddns.net:25565">
+                サーバーを追加する<br>(統合版)
+              </v-btn>
+              <p>
+                ◆追加されないときは以下のようにしてください◆<br>
+                <span>
+                  サーバー名「好きな名前」
+                  <br>
+                  サーバーアドレス「waka1210mc.ddns.net」
+                  <br>
+                  ポート「25565」
+                </span>
+              </p>
+              <v-img width="700px" src="@/assets/Minecraft/BEAddrPort.png" size="x-large"/>
+            </v-col>
+          </v-row>
+          <v-row class="d-flex align-center justify-center">
+              <v-col cols="auto"><!--Java版-->
+                <p>
+                ◆Java版は以下のようにしてください◆<br>
+                <span>
+                  サーバー名「好きな名前」
+                  <br>
+                  サーバーアドレス「waka1210mc.ddns.net」
+                  <br>
+                  ポート「25565」
+                </span>
+              </p>
+              <v-img width="700px" src="@/assets/Minecraft/JAVAAddrPort.png" size="x-large"/>
+              </v-col>
+          </v-row>
+        </div>
         <div class="py-5" />
         <v-expansion-panels>
             <v-expansion-panel>
